e53b5fd27d242d6c222c9365f3c9080507b82b6e
Maciej Zboch <maciejzboch@gmail.com>
2025-09-04 17:31:43 +0200

    fix: move footer to a separate component

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,12 +1,10 @@
 import { Suspense } from "react";
-import { useEffect, useState } from "react";
-import {ITea} from "../types/tea";
-import { IPopulatedReview } from "@/types/review";
 import LatestTeas from "../components/LatestTeas";
 import LatestReviews from "@/components/LatestReviews";
 import LatestTeasSkeleton from "@/components/LatestTeasSkeleton";
 import LatestReviewsSkeleton from "@/components/LatestReviewSkeleton";
-const API_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+import Footer from "@/components/Footer";
+
 
 export default function HomePage() {
   return (
@@ -36,9 +34,7 @@ export default function HomePage() {
       </main>
 
       {/* Footer */}
-      <footer className="px-6 py-6 bg-dark text-center text-sm text-ash">
-        © {new Date().getFullYear()} The Pu-Erh Catalogue.
-      </footer>
+<Footer/>
     </div>
   );
 }
---
+++ frontend/src/components/Footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+          return (<footer className="px-6 py-6 bg-dark text-center text-sm text-ash">
+        © {new Date().getFullYear()} The Pu-Erh Catalogue.
+      </footer>)
+}
