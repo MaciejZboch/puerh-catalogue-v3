f0489689426e0f3be1a536ad6e826aa98dc73d64
Maciej Zboch <maciejzboch@gmail.com>
2025-09-30 17:42:52 +0200

    styling

--- frontend/src/app/tea/[id]/page.tsx
+++ frontend/src/app/tea/[id]/page.tsx
@@ -20,8 +20,8 @@ export default async function TeaPage({ params }: { params: { id: string } }) {
   const currentUser = await getCurrentUserForServer();
   return (
     <main className="max-full mx-auto p-6 grid grid-cols-1 lg:grid-cols-3 gap-8 bg-dark text-light">
-      {/* Left Column */}
-      <div className="lg:col-span-1 flex flex-col items-center">
+      {/* Left Column - tea card */}
+      <div className="lg:col-span-1 flex flex-col items-center bg-charcoal rounded-md border-b border-green-accent">
 <Image
   src={
     tea.images?.[1]?.url ||
