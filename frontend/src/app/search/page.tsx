bf97e8b12144de2687d2e84c1909d97e5cf568e1
Maciej Zboch <maciejzboch@gmail.com>
2025-11-20 18:48:49 +0100

    fix: issue with suspense on search page

--- frontend/src/app/search/page.tsx
+++ frontend/src/app/search/page.tsx
@@ -3,7 +3,9 @@ import Search from "@/components/Search";
 import { Suspense } from "react";
 
 export default function SearchPage() {
+   return (
   <Suspense fallback="Loading...">
    <Search/>
   </Suspense>
+   );
 }
