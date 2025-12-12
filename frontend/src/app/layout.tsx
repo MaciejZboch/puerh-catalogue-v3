3b513c917885ae596c8033330ec65353cbc70bed
Maciej Zboch <maciejzboch@gmail.com>
2025-12-12 13:43:29 +0100

    fix: adjust layout

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col bg-dark`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased flex flex-col bg-dark min-h-screen`}
       >
         <AuthProvider>
         <Navbar/>
