f072ca029b881d7ebf7cdd702e887617631eace2
Maciej Zboch <maciejzboch@gmail.com>
2025-12-26 20:03:23 +0100

    feat: add privacy policy

---
+++ frontend/src/app/privacy/page.tsx
@@ -0,0 +1,55 @@
+export default function PrivacyPolicyPage() {
+  return (
+    <main className="w-full mx-auto px-6 py-16 text-light bg-dark ">
+      <div className="sm:w-2xl md:w-3xl mx-auto px-6 py-16 text-justify">
+        <h1 className="text-3xl font-bold mb-4 text-center">Privacy Policy</h1>
+        <p>
+          We take your privacy seriously. To better protect your privacy we
+          provide this privacy policy notice explaining the way your personal
+          information is collected and used.
+        </p>{" "}
+        <br />
+        <h2 className="font-bold text-xl text-center mb-4">
+          Collection of Routine Information
+        </h2>
+        <p>
+          This website tracks basic information about its users. This
+          information includes, but is not limited to, IP addresses, browser
+          details, timestamps and referring pages. None of this information can
+          personally identify specific users to this website. The information is
+          tracked for routine administration and maintenance purposes.
+        </p>{" "}
+        <br />
+        <h2 className="font-bold text-xl text-center mb-4">Cookies</h2>
+        <p>
+          Where necessary, this website uses cookies to store information about
+          a visitor’s preferences and history in order to better serve the user
+          and/or present the user with customized content.
+        </p>{" "}
+        <br />
+        <h2 className="font-bold text-xl text-center mb-4">Security</h2>
+        <p>
+          The security of your personal information is important to us, but
+          remember that no method of transmission over the Internet, or method
+          of electronic storage, is 100% secure. While we strive to use
+          commercially acceptable means to protect your personal information, we
+          cannot guarantee its absolute security.
+        </p>
+        <br />
+        <h2 className="font-bold text-xl text-center mb-4">
+          Changes To This Privacy Policy
+        </h2>
+        <p>
+          This Privacy Policy is effective as of 27.12.2025 and will remain in
+          effect except with respect to any changes in its provisions in the
+          future, which will be in effect immediately after being posted on this
+          page. We reserve the right to update or change our Privacy Policy at
+          any time and you should check this Privacy Policy periodically. If we
+          make any material changes to this Privacy Policy, we will notify you
+          either through the email address you have provided us, or by placing a
+          prominent notice on our website.
+        </p>
+      </div>
+    </main>
+  );
+}
--- frontend/src/components/global/Footer.tsx
+++ frontend/src/components/global/Footer.tsx
@@ -1,7 +1,13 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="px-6 py-6 bg-dark text-center text-sm text-ash">
-      © {new Date().getFullYear()} The Pu-Erh Catalogue.
+      <span>© {new Date().getFullYear()} The Pu-Erh Catalogue</span>
+      <span className="mx-2">•</span>
+      <Link href="/privacy" className="text-ash">
+        Privacy Policy
+      </Link>
     </footer>
   );
 }
