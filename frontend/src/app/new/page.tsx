17ed59d57004f32dc013ddf2341a38b2e54ce791
Maciej Zboch <maciejzboch@gmail.com>
2025-12-31 18:54:43 +0100

    fix: set runtime to nodejs

--- frontend/src/app/new/page.tsx
+++ frontend/src/app/new/page.tsx
@@ -2,6 +2,9 @@ import { redirect } from "next/navigation";
 import { getCurrentUserForServerComponents } from "@/lib/auth.server";
 import New from "@/components/new/New";
 
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
 export default async function NewTeaPage() {
   const user = await getCurrentUserForServerComponents();
   if (!user) redirect("/");
